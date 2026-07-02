b8f9d66ac9b10779eb2dd63535395ee7a5d3d44f
MOHITJOSHI2 <joshimohit22552@gmail.com>
2026-07-02 22:16:41 +0545

    form changes

--- controllers/formControllers/saveFormData.js
+++ controllers/formControllers/saveFormData.js
@@ -3,10 +3,10 @@ const temporaryFormData = require("../../models/formModel/temporaryFormModel");
 exports.uploadFile = async (req, res) => {
     try {
         //Access Service Provider Details
-        const { name, email, phone, designation } = req.body;
+        const { name, email, phone, designation, wsucName } = req.body;
 
         // Access Location Data
-        const { province, district, municipality, wardsCovered, coordinates, service_coverage, adequacy, water_quality, reliability, non_revenue_water, operating_ratio, metering_ratio, grievances_addressal } = req.body
+        const { province, district, municipality, wardsCovered, latitude, longitude, service_coverage, adequacy, water_quality, reliability, non_revenue_water, operating_ratio, metering_ratio, grievances_addressal } = req.body
 
         // Access uploaded files
         const documents = req.files.map(file => ({
@@ -19,6 +19,8 @@ exports.uploadFile = async (req, res) => {
         }));
 
         const operatorData = {
+            WSUC_Name: wsucName,
+
             //Operator Personal Info
             Operator_Info: {
                 Name: name,
@@ -38,58 +40,83 @@ exports.uploadFile = async (req, res) => {
             // Main Data for Calculating rhe actual Scores
             Service_Coverage_Score: {
                 KPI_1: service_coverage.KPI_1,
+                KPI_1_Remark: service_coverage.KPI_1_Remark,
                 KPI_2: service_coverage.KPI_2,
-                KPI_3: service_coverage.KPI_3
+                KPI_2_Remark: service_coverage.KPI_2_Remark,
+                KPI_3: service_coverage.KPI_3,
+                KPI_3_Remark: service_coverage.KPI_3_Remark
             },
 
             Adequacy_Score: {
                 KPI_1: adequacy.KPI_1,
-                KPI_2: adequacy.KPI_2
+                KPI_1_Remark: adequacy.KPI_1_Remark,
+                KPI_2: adequacy.KPI_2,
+                KPI_2_Remark: adequacy.KPI_2_Remark,
             },
 
             Water_Quality_Score: {
                 KPI_1: water_quality.KPI_1,
+                KPI_1_Remark: water_quality.KPI_1_Remark,
                 KPI_2: water_quality.KPI_2,
+                KPI_2_Remark: water_quality.KPI_2_Remark,
                 KPI_3: water_quality.KPI_3,
+                KPI_3_Remark: water_quality.KPI_3_Remark,
                 KPI_4: water_quality.KPI_4,
-                KPI_5: water_quality.KPI_5
+                KPI_4_Remark: water_quality.KPI_4_Remark,
+                KPI_5: water_quality.KPI_5,
+                KPI_5_Remark: water_quality.KPI_5_Remark,
 
             },
 
             Reliability_Score: {
                 KPI_1: reliability.KPI_1,
-                KPI_2: reliability.KPI_2
+                KPI_1_Remark: reliability.KPI_1_Remark,
+                KPI_2: reliability.KPI_2,
+                KPI_2_Remark: reliability.KPI_2_Remark,
             },
 
             NRW_Score: {
                 KPI_1: non_revenue_water.KPI_1,
+                KPI_1_Remark: non_revenue_water.KPI_1_Remark,
                 KPI_2: non_revenue_water.KPI_2,
+                KPI_2_Remark: non_revenue_water.KPI_2_Remark,
                 KPI_3: non_revenue_water.KPI_3,
+                KPI_3_Remark: non_revenue_water.KPI_3_Remark,
                 KPI_4: non_revenue_water.KPI_4,
+                KPI_4_Remark: non_revenue_water.KPI_4_Remark,
                 KPI_5: non_revenue_water.KPI_5,
-                KPI_6: non_revenue_water.KPI_6
+                KPI_5_Remark: non_revenue_water.KPI_5_Remark,
+                KPI_6: non_revenue_water.KPI_6,
+                KPI_6_Remark: non_revenue_water.KPI_6_Remark,
             },
 
             OM_Score: {
                 KPI_1: operating_ratio.KPI_1,
+                KPI_1_Remark: operating_ratio.KPI_1_Remark,
                 KPI_2: operating_ratio.KPI_2,
-                KPI_3: operating_ratio.KPI_3
+                KPI_2_Remark: operating_ratio.KPI_2_Remark,
+                KPI_3: operating_ratio.KPI_3,
+                KPI_3_Remark: operating_ratio.KPI_3_Remark,
             },
 
             Metering_Ratio_Score: {
                 KPI_1: metering_ratio.KPI_1,
-                KPI_2: metering_ratio.KPI_2
+                KPI_1_Remark: metering_ratio.KPI_1_Remark,
+                KPI_2: metering_ratio.KPI_2,
+                KPI_2_Remark: metering_ratio.KPI_2_Remark,
             },
 
             Grievance_Score: {
                 KPI_1: grievances_addressal.KPI_1,
-                KPI_2: grievances_addressal.KPI_2
+                KPI_1_Remark: grievances_addressal.KPI_1_Remark,
+                KPI_2: grievances_addressal.KPI_2,
+                KPI_2_Remark: grievances_addressal.KPI_2_Remark,
             },
 
             //Coordinates of project covered area
             geometry: {
-                type: "Polygon",
-                coordinates: coordinates
+                lat: latitude,
+                lng: longitude
             },
 
             //All the affiliated data files related to the projects
--- models/formModel/temporaryFormModel.js
+++ models/formModel/temporaryFormModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
 const temporaryFormSchema = new mongoose.Schema({
+    WSUC_Name: { type: String },
+
     Status: {
         type: String,
         enum: ["Pending", "Approved", "Rejected"],
@@ -30,65 +32,82 @@ const temporaryFormSchema = new mongoose.Schema({
     // Main Data for Calculating rhe actual Scores
     Service_Coverage_Score: {
         KPI_1: { type: String },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: Number },
-        KPI_3: { type: Number }
+        KPI_2_Remark: { type: String },
+        KPI_3: { type: Number },
+        KPI_3_Remark: { type: String },
     },
 
     Adequacy_Score: {
         KPI_1: { type: Number },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: Number },
+        KPI_2_Remark: { type: String }
     },
 
     Water_Quality_Score: {
         KPI_1: { type: String },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: String },
+        KPI_2_Remark: { type: String },
         KPI_3: { type: String },
+        KPI_3_Remark: { type: String },
         KPI_4: { type: String },
+        KPI_4_Remark: { type: String },
         KPI_5: { type: String },
+        KPI_5_Remark: { type: String },
 
     },
 
     Reliability_Score: {
         KPI_1: { type: String },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: Number },
+        KPI_2_Remark: { type: String },
     },
 
     NRW_Score: {
         KPI_1: { type: String },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: Number },
+        KPI_2_Remark: { type: String },
         KPI_3: { type: Number },
+        KPI_3_Remark: { type: String },
         KPI_4: { type: Number },
+        KPI_4_Remark: { type: String },
         KPI_5: { type: String },
+        KPI_5_Remark: { type: String },
         KPI_6: { type: String },
+        KPI_6_Remark: { type: String },
     },
 
     OM_Score: {
         KPI_1: { type: String },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: Number },
+        KPI_2_Remark: { type: String },
         KPI_3: { type: Number },
+        KPI_3_Remark: { type: String },
     },
 
     Metering_Ratio_Score: {
         KPI_1: { type: Number },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: Number },
+        KPI_2_Remark: { type: String },
     },
 
     Grievance_Score: {
         KPI_1: { type: String },
+        KPI_1_Remark: { type: String },
         KPI_2: { type: String },
+        KPI_2_Remark: { type: String },
     },
 
     geometry: {
-        type: {
-            type: String,
-            enum: ["Polygon", "MultiPolygon"],
-            required: true,
-        },
-
-        coordinates: {
-            type: Array,
-            required: true,
-        },
+        lat: { type: Number },
+        lng: { type: Number }
     },
 
     Documents: {
